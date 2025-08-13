08497548b662356fd5c3b9f6baea457732b6fc88
rezajatnika <reza.jatnika@protonmail.com>
2025-08-13 16:15:07 +0700

    Improve `ex_task` to send UDP messages; integrate `system_init` in `main`.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "src/scheduler.h"
@@ -6,18 +7,20 @@
 #include "src/udp.h"
 
 void ex_task(void *data) {
-    for (int i = 0; i < 10; i++) {
-        __asm__ __volatile__("nop");
-    }
+    udp_t *udp = (udp_t *)data;
+    char buf[128];
+    snprintf(buf, sizeof(buf), "%s, %d\n", udp->host, millis());
+    udp_send(udp, buf, strlen(buf));
 }
 
 int main(void) {
     sched_t sched;
     udp_t udp;
+    system_init();
     sched_init(&sched, 4);
     udp_init(&udp, "127.0.0.1", 8000);
 
-    sched_add_task(&sched, ex_task, "1234", 1000, 0 ,"ex_task");
+    sched_add_task(&sched, ex_task, &udp, 1000, 0 ,"ex_task");
     sched_set_log_hook(&sched, NULL);
 
     sched_setup_signal_handlers();
