401304632f3f02280d0ae86d2e10bf1afd7cab4b
rezajatnika <reza.jatnika@protonmail.com>
2025-08-14 16:04:30 +0700

    Add static library for core and config modules.
    - Built a static library (`librjos.a`) to encapsulate core functionalities and configuration handlers.
    - Modularized the project by separating core logic (`core`) and configuration logic (`config`) into reusable components.
    - Updated `CMakeLists.txt` to build and link the static library.
    - Improved flexibility and maintainability by enabling reuse of `core` and `config` across executables.

---
+++ main_udp.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include "src/config.h"
+#include "src/system.h"
+#include "src/udp.h"
+
+int main(void) {
+    /* System initialization. */
+    system_init();
+
+    /* Configuration initialization. */
+    config_t config;
+    config_init(&config);
+
+    if (config_load(&config, "ex_config.txt") != 0) {
+        return -1;
+    }
+    const char *host = config_get(&config, "host");
+    const char *port = config_get(&config, "port");
+
+    udp_t udp;
+    udp_init(&udp, host, atoi(port));
+    for (int i = 0; i < 10; ++i)
+        udp_send(&udp, "Hello, world!\n", 14);
+
+    return 0;
+}
